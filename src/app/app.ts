a33e5bff56761bc27f11c00e171b14a424851847
AliciaM05 <aliciarotru@gmail.com>
2026-04-02 15:09:41 +0100

    Foro arreglado responsive

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { RegistroMain } from './componentes/registro/registro-main/registro-main';
 import { CatalogoBody } from './componentes/catalog-body/catalog-body';
 import { PerfilCompleto } from './componentes/perfil-body/perfil-body';
+import { ForoBody } from "./componentes/foro/foro-body/foro-body";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegistroMain, CatalogoBody, PerfilCompleto],
+  imports: [RouterOutlet, RegistroMain, CatalogoBody, PerfilCompleto, ForoBody],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
