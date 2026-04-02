b24268227bcec8b8d59ced34b40fed3edec24568
Kilian Santana Delgado <kiliandelgado2113@gmail.com>
2026-04-02 15:47:42 +0100

    Configuración de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { RegistroMain } from './componentes/registro/registro-main/registro-main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'home', component: Home },
+  { path: 'registro', component: RegistroMain },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegistroMain } from './componentes/registro/registro-main/registro-main';
-import { CatalogoBody } from './componentes/catalog-body/catalog-body';
-import { PerfilCompleto } from './componentes/perfil-body/perfil-body';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegistroMain, CatalogoBody, PerfilCompleto],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
