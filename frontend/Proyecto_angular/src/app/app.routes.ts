7d0f4b21259d4fa4b5aefcf9ccbb55ab4e02e7b3
jose <jose.ojeda120@alu.ulpgc.es>
2026-04-03 21:24:15 +0100

    Cooregido formatos

--- frontend/Proyecto_angular/src/app/app.routes.ts
+++ frontend/Proyecto_angular/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { RegistroMain } from './componentes/registro/registro-main/registro-main';
+import { ForoBody } from './componentes/foro/foro-body/foro-body';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'home', component: Home },
+  { path: 'registro', component: RegistroMain },
+  { path: 'foro', component: ForoBody },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
