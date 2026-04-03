785d5cb2216735cb3099b92823a5140702938e77
jose <jose.ojeda120@alu.ulpgc.es>
2026-04-03 19:54:40 +0100

    Cambios

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { RegistroMain } from './componentes/registro/registro-main/registro-main
 import { CatalogoBody } from './componentes/catalog-body/catalog-body';
 import { PerfilCompleto } from './componentes/perfil-body/perfil-body';
 import { ForoBody } from "./componentes/foro/foro-body/foro-body";
+import { Social } from './componentes/Social/social';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegistroMain, CatalogoBody, PerfilCompleto, ForoBody],
+  imports: [RouterOutlet, RegistroMain, CatalogoBody, PerfilCompleto, ForoBody, Social],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
