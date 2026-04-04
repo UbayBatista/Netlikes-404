20c0fe1ebaf45f46ffa95dde96fca046cb3c300e
AliciaM05 <aliciarotru@gmail.com>
2026-04-04 12:00:39 +0100

    Foro imagenes usuario añadidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
 import { RegistroMain } from './componentes/registro/registro-main/registro-main';
 import { ForoBody } from './componentes/foro/foro-body/foro-body';
+import { Social } from './componentes/Social/social';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'home', component: Home },
   { path: 'registro', component: RegistroMain },
   { path: 'foro', component: ForoBody },
+  { path: 'social', component: Social },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
